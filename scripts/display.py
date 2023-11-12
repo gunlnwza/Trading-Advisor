f2305daf6b803235c3d02412169d20329a0f02f7
Natachat Teecharcharn <techazuza@gmail.com>
2023-11-12 23:57:17 +0700

    completed show_future() with the help of ChatGPT

--- scripts/display.py
+++ scripts/display.py
@@ -22,21 +22,23 @@ def show_future(model, price_df, x="datetime", y="close", title="", points=10):
 
     plt.figure(title, figsize=(8, 6))
 
-    plt.plot(price_df[y])
+    plt.plot(price_df[y], label="Actual Data")
 
-    x = []
-    y = []
-    close = price_df["close"].iloc[-1]
-    for i in range(-1, points):
-        x.append(len(price_df) + i)
-        y.append(close)
+    future_predictions = []
 
-        close = model.predict(pd.DataFrame({"close": [close]}))
+    last_close = price_df["close"].iloc[-1]
 
-    plt.plot(x, y)
+    for i in range(points):
+        future_predictions.append(last_close)
+        last_close = model.predict(pd.DataFrame({"close": [last_close]}))[0]
 
+    future_indices = [i - 1 for i in range(len(price_df), len(price_df) + points)]
+
+    plt.plot(future_indices, future_predictions, label="Predicted Data", linestyle="--")
     plt.xticks(rotation=30)
     plt.title(title)
+    plt.title(title)
+    plt.legend()
 
     fig_manager = plt.get_current_fig_manager()
     fig_manager.window.wm_geometry("+500+100")
@@ -50,6 +52,4 @@ if __name__ == "__main__":
     data = load_data()
     df = make_df(data)
     model = get_model(df)
-    print(df)
-
     show_future(model, df)
